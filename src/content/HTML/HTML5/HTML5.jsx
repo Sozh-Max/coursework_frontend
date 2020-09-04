45131502671ef22dd8ea1a579762f0b809e225e4
Kazachenka Maksim <sozhskifront@gmail.com>
2020-09-04 08:56:12 +0300

    create html5, but this don't works

--- src/content/HTML/HTML5/HTML5.jsx
+++ src/content/HTML/HTML5/HTML5.jsx
@@ -1,48 +1,16 @@
 import React from 'react';
+import parse from 'html-react-parser';
 
 import Box from '@material-ui/core/Box';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
 
+import { textTeg } from './utils';
 
 export const HTML5 = () => {
 
   return (
     <Box>
-        <Table
-          aria-label="simple table"
-        >
-          <TableHead>
-            <TableRow>
-              <TableCell> Заголовок 1 </TableCell>
-              <TableCell> Заголовок 2 </TableCell>
-              <TableCell> Заголовок 3 </TableCell>
-              <TableCell> Заголовок 4 </TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            <TableRow>
-              <TableCell> Текст </TableCell>
-              <TableCell> Текст </TableCell>
-              <TableCell rowspan='2'> Объединили ряды </TableCell>
-              <TableCell> Текст </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell> Текст </TableCell>
-              <TableCell> Текст </TableCell>
-              <TableCell> Текст </TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell> Текст </TableCell>
-              <TableCell colspan='2'> Объединили колонки </TableCell>
-              <TableCell> Текст </TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-    </Box>
+      {parse(textTeg)}
+     </Box>
   )
 }
 
---
+++ src/content/HTML/HTML5/utils.js
@@ -0,0 +1,3 @@
+export const textTeg = `
+    <code> <pre> <table aria-label=\"simple table\"> <thead> <tr> <th> Заголовок 1 </th> <th> Заголовок 2 </th> <th> Заголовок 3 </th> <th> Заголовок 4 </th> </tr> </thead> <tbody> <tr> <td> Текст </td> <td> Текст </td> <td rowspan='2'> Объединили ряды </td> <td> Текст </td> </tr> <tr> <td> Текст </td> <td> Текст </td> <td> Текст </td> </tr> <tr> <td> Текст </td> <td colspan='2'> Объединили колонки </td> <td> Текст </td> </tr> </tbody> </table> </pre> </code>
+`
